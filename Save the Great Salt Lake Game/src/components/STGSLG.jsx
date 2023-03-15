387e77c1e3687f8b695a01c276fd13d79e4a1608
L1lith <lily@l1lith.com>
2023-03-15 09:25:31 -0600

    add partial support for other people's responses

--- Save the Great Salt Lake Game/src/components/STGSLG.jsx
+++ Save the Great Salt Lake Game/src/components/STGSLG.jsx
@@ -9,12 +9,12 @@ export default function STGSLG() {
 		e.preventDefault()
 		if (disabled()) return
 		setDisabled(true)
-		setHistory(history().concat([await processInput(input.value)]))
+		setHistory(history().concat([{message: await processInput(input.value), author: 'user'}]))
 		input.value = ""
 		setDisabled(false)
 	}
 	const processInput = async (val) => {
 		return val
 	}
-	return <form onSubmit={doSubmit} class="stgslg"><div class="history">{history().map(value => (<span class="event">{value}</span>))}</div><input ref={input} class="response"></input></form>
+	return <form onSubmit={doSubmit} class="stgslg"><div class="history">{history().map(value => (<span class={"event " + (value.author === "user" ? 'own' : 'other')}>{value.message}</span>))}</div><input ref={input} class="response"></input></form>
 }
