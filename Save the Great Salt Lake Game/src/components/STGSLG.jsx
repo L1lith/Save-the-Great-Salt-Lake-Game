eca757e86290ffb25a3608a894a966a48ac6a46a
L1lith <lily@l1lith.com>
2023-03-15 09:22:37 -0600

    create my history terminal

--- Save the Great Salt Lake Game/src/components/STGSLG.jsx
+++ Save the Great Salt Lake Game/src/components/STGSLG.jsx
@@ -1,7 +1,20 @@
+import "../styles/STGSLG.scss"
+import {createSignal} from 'solid-js'
+
 export default function STGSLG() {
-	const doSubmit = (e)=>{
+	const [history, setHistory] = createSignal([])
+	let input
+	const [disabled, setDisabled] = createSignal(false)
+	const doSubmit = async (e)=>{
 		e.preventDefault()
-		alert('hi')
+		if (disabled()) return
+		setDisabled(true)
+		setHistory(history().concat([await processInput(input.value)]))
+		input.value = ""
+		setDisabled(false)
 	}
-	return <form onSubmit={doSubmit} class="stgslg"><input class="response"></input></form>
+	const processInput = async (val) => {
+		return val
+	}
+	return <form onSubmit={doSubmit} class="stgslg"><div class="history">{history().map(value => (<span class="event">{value}</span>))}</div><input ref={input} class="response"></input></form>
 }
